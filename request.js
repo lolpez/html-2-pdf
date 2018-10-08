b1894c49ef262ecaa32b5bb21f296274534a8ce1
lolpez <lopezsandi1666@gmail.com>
2018-10-08 17:10:50 -0400

    Added a request method to test AWS

---
+++ request.js
@@ -0,0 +1,19 @@
+var request = require('request');
+var url = "https://etkv80dh9b.execute-api.us-east-2.amazonaws.com/default/test";
+var fs = require('fs');
+var html = fs.readFileSync('./index.html', 'utf8');
+
+var options = {
+	url: url,
+	method: 'post',
+	json: {
+		name: "hello world",
+		html: html
+	}
+};
+
+request(options, function (error, response, body) {
+	console.log('error:', error); // Print the error if one occurred
+	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	console.log('body:', body); // Print the HTML for the Google homepage.
+});
