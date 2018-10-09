dca3a879141a3f83055448209f47e7874280d3a8
lolpez <lopezsandi1666@gmail.com>
2018-10-09 10:40:01 -0400

    Added buffer method

--- buffer.js
+++ buffer.js
@@ -3,7 +3,8 @@ var pdf = require('html-pdf');
 var html = fs.readFileSync('./index.html', 'utf8');
 var options = { format: 'Letter' };
 
-pdf.create(html, options).toFile('./files/generated.pdf', function (err, res) {
+pdf.create(html, options).toBuffer(function (err, buffer) {
 	if (err) return console.log(err);
-	console.log(`PDF generated at: "${res.filename}"`);
+	console.log(`PDF buffered: "${Buffer.from(buffer)}"`);
 });
+
