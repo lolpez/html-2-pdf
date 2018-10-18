72c03013ea1719138fc5be6458fd57008aeae0a4
lolpez <lopezsandi1666@gmail.com>
2018-10-18 16:37:36 -0400

    Code refactoring

--- request.js
+++ request.js
@@ -1,19 +1,28 @@
 var request = require('request');
-var url = "https://etkv80dh9b.execute-api.us-east-2.amazonaws.com/default/html2pdf";
 var fs = require('fs');
+var path = require("path");
+var url = "https://17x4yl3c71.execute-api.us-east-2.amazonaws.com/Develop/html-to-pdf-OS";
 var html = fs.readFileSync('./index.html', 'utf8');
+var filename = "converted.pdf";
 
 var options = {
 	url: url,
 	method: 'post',
 	json: {
-		name: "hello world",
-		html: html
+		html_base64: html
 	}
 };
 
 request(options, function (error, response, body) {
-	console.log('error:', error); // Print the error if one occurred
-	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-	console.log('body:', body); // Print the HTML for the Google homepage.
+	if (error){
+		console.log('Request error:', error);
+	}else{
+		fs.writeFile(`./${filename}`, body.pdf_base64, 'base64', function(err) {
+			if (err){
+				console.log("Error trying to convert base64 string to PDF file.")
+			}else{
+				console.log(`PDF downloaded at: "${path.resolve(`./${filename}`)}"`);
+			}
+		});
+	}
 });
